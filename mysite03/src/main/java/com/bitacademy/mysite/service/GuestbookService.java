e243634817e48044c53f447467880196811d75cd
tjdgns0029 <whtjdgns0029@naver.com>
2022-11-16 17:27:14 +0900

    mysite

---
+++ mysite03/src/main/java/com/bitacademy/mysite/service/GuestbookService.java
@@ -0,0 +1,18 @@
+package com.bitacademy.mysite.service;
+
+import java.util.List;
+
+import com.bitacademy.mysite.vo.GuestbookVo;
+
+public class GuestbookService {
+
+	public List<GuestbookVo> getContentsList() {
+		return null;
+	}
+	
+	public void deleteContentsList(Long no, String password) {
+	}
+	
+	public void addContentsList(GuestbookVo vo) {
+	}
+}
